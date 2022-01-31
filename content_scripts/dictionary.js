24a869e5e47effdbaf3dc5a6101942c680e3f536
Julio Sergio <juliosergio010302@gmail.com>
2022-01-30 23:12:50 -0300

    Make the popup's layout

---
+++ content_scripts/dictionary.js
@@ -0,0 +1,10 @@
+(function () {
+  /**
+   * Check and set a global guard variable.
+   * If this content script is injected into the same page again,
+   * it will do nothing next time.
+   */
+  if (window.hasRun) {
+    return;
+  }
+})();
