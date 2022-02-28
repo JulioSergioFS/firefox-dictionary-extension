b9a2f185232561f44dec9a501865ec8b6d2d158c
Julio Sergio <juliosergio010302@gmail.com>
2022-02-28 17:41:48 -0300

    Create fetch function to get word definition

--- content_scripts/dictionary.js
+++ content_scripts/dictionary.js
@@ -1,14 +1,20 @@
-browser.windows.getCurrent({ populate: true }).then((windowInfo) => {
-  myWindowId = windowInfo.id;
+function searchWord() {
+  fetch("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
+    .then((response) => response.json())
+    .then(
+      (data) => (document.getElementById("testeid").innerHTML = "ssssssss")
+    );
+}
 
-  document.getElementById("search").addEventListener("click", () => {
-    const Http = new XMLHttpRequest();
-    const url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello";
-    Http.open("GET", url);
-    Http.send();
+document.addEventListener("DOMContentLoaded", function () {
+  const searchButton = document.getElementById("search-button");
 
-    Http.onreadystatechange = (e) => {
-      document.getElementById("testeid").textContent = Http.responseText;
-    };
+  searchButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
+      .then((response) => response.json())
+      .then(
+        (data) => (document.getElementById("testeid").innerHTML = data[0].word)
+      );
   });
 });
